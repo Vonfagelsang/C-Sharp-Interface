c17f9ebca385852a248c406f060e65dfc0d44e61
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-10-04 10:36:11 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,23 +10,23 @@
         ShapeList.Add(new Rectangle());
         ShapeList.Add(new Circle());
 
-        Console.WriteLine("Choose 1 for Triangle or 2 for Rectangle or three for Cicrle");
+        Console.WriteLine("Choose 1 for Triangle or 2 for Rectangle or 3 for Circle");
         awnser = double.Parse(Console.ReadLine());
 
         if(awnser == 1)
         {
-            Console.WriteLine("Area: " + ShapeList[0].Area());
-            Console.WriteLine("Circumferance: " + ShapeList[0].Circumferance());
+            Console.WriteLine("Triangle area: " + ShapeList[0].Area());
+            Console.WriteLine("Triangle circumferance: " + ShapeList[0].Circumferance());
         }
         else if(awnser == 2)
         {
-            Console.WriteLine("Area: " + ShapeList[1].Area());
-            Console.WriteLine("Circumferance: " + ShapeList[1].Circumferance());
+            Console.WriteLine("Rectangle area: " + ShapeList[1].Area());
+            Console.WriteLine("Rectangle circumferance: " + ShapeList[1].Circumferance());
         }
         else if(awnser == 3)
         {
-            Console.WriteLine("Area: " + ShapeList[2].Area());
-            Console.WriteLine("Circumferance: " + ShapeList[2].Circumferance());
+            Console.WriteLine("Circle area: " + ShapeList[2].Area());
+            Console.WriteLine("Circle circumferance: " + ShapeList[2].Circumferance());
         }
     }
 }
