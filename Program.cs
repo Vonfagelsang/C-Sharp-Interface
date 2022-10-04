31c33361511179537fc96589a3c23721040bc372
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-10-04 10:21:23 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,15 +4,13 @@
     {
         Console.Clear();
         double awnser;
-        Console.WriteLine("Write the width");
-        double width = double.Parse(Console.ReadLine());
-        Console.WriteLine("Write  the height");
-        double height = double.Parse(Console.ReadLine());
         
         List<IShape> ShapeList = new List<IShape>();
         ShapeList.Add(new Triangle());
         ShapeList.Add(new Rectangle());
-        Console.WriteLine("Choose 1 for Triangle or 2 for Rectangle");
+        ShapeList.Add(new Circle());
+
+        Console.WriteLine("Choose 1 for Triangle or 2 for Rectangle or three for Cicrle");
         awnser = double.Parse(Console.ReadLine());
 
         if(awnser == 1)
@@ -24,6 +22,11 @@
         {
             Console.WriteLine("Area: " + ShapeList[1].Area());
             Console.WriteLine("Circumferance: " + ShapeList[1].Circumferance());
+        }
+        else if(awnser == 3)
+        {
+            Console.WriteLine("Area: " + ShapeList[2].Area());
+            Console.WriteLine("Circumferance: " + ShapeList[2].Circumferance());
         }
     }
 }
